fa412e24914fc8ea26213f72e31e17e472cad19e
MohammadMahdi Taheri <mmtaheri@yandex.com>
2022-03-23 12:31:06 +0400

    + implement register method

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password']),
+        ]);
+
+        $accessToken = $user->createToken('apiapp');
+
+        $response = [
+            'user' => $user,
+            'token' => $accessToken
+        ];
+
+        return response($response, 201);
+    }
+}
