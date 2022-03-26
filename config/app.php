b3c0bbc1f7978f3f7cfccac0e9a26669f78e76a4
MohammadMahdi Taheri <mmtaheri@yandex.com>
2022-03-26 14:58:50 +0400

    + register userRepositoryServiceProvider

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\ProductRepositoryServiceProvider::class,
+        App\Providers\UserRepositoryServiceProvider::class,
 
     ],
 
